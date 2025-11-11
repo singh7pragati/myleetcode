d0e11d448685c98d9146fc2031c43f4fc740d387
singh7pragati <singh7pragati@gmail.com>
2025-11-11 19:06:34 +0530

    .

---
+++ 103 Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,65 @@
+103. Binary Tree Zigzag Level Order Traversal
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>result;
+        queue <TreeNode*> q;
+        
+        if(root == NULL){
+            return result;
+        }
+
+        bool LeftToRight = true;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> ans(size);
+
+            for(int i = 0; i < size; i++){
+                    TreeNode* frontNode = q.front();
+                    q.pop();
+
+                    int index = LeftToRight? i : size - i - 1;
+                    ans[index] = frontNode -> val;
+
+                    if(frontNode -> left){
+                        q.push(frontNode -> left);
+                    }
+
+                    if(frontNode -> right){
+                        q.push(frontNode -> right);
+                    }
+            }
+            LeftToRight = !LeftToRight;
+            
+            result.push_back(ans);     
+        }
+        return result;
+    }
+};
